2a809caceedf137d424357c9c14d5c38a88c34d0
Felix Wetell <felix.wetell@gmail.com>
2018-11-02 21:34:37 +0100

    Added some CSS and a navbar templete

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,8 +14,25 @@
 		<link rel="icon" href="{{ asset( '/img/logo.ico' ) }}">
 	</head>
 	<body>
-        <nav>
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Link</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
         <div class="container">
             @yield( 'content' )
