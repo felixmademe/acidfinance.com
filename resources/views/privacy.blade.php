2e5cf5296913afdc60ef2e03ef0869000fb45a3f
Felix Wetell <felix.wetell@gmail.com>
2018-11-12 00:56:00 +0100

    Added a privacy policy view

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section( 'content' )
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="text-center">
+                <h1>Privacy Policy</h1>
+            </div>
+            <hr>
+            <p>
+
+            </p>
+        </div>
+    </div>
+
+@endsection
