0d5768a869035f27c0b6c3c4c886a7d96548bf1a
Felix Wetell <felix.wetell@gmail.com>
2018-11-12 12:29:01 +0100

    Formatted some code

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -37,7 +37,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware( 'guest' );
     }
 
     /**
@@ -48,11 +48,12 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'firstName' => ['required', 'string', 'max:255'],
-            'lastName' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        return Validator::make( $data, [
+            'username'  => [ 'required', 'string', 'max:60', 'unique:users' ],
+            'firstName' => [ 'required', 'string', 'max:60' ],
+            'lastName'  => [ 'required', 'string', 'max:60' ],
+            'email'     => [ 'required', 'string', 'email', 'max:255', 'unique:users' ],
+            'password'  => [ 'required', 'string', 'min:6', 'confirmed' ],
         ]);
     }
 
@@ -62,12 +63,14 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create( array $data )
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+        return User::create( [
+            'username'  => $data[ 'username' ],
+            'firstName' => $data[ 'firstName' ],
+            'lastName'  => $data[ 'lastName' ],
+            'email'     => $data[ 'email' ],
+            'password'  => Hash::make( $data[ 'password' ] ),
         ]);
     }
 }
