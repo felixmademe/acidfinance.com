54b8786c50d5508014dad1f524b7a4f646223037
Felix Wetell <felix.wetell@gmail.com>
2018-11-13 19:41:39 +0100

    Added new view and routed it

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get( 'index', function ()
 {
@@ -31,6 +32,7 @@ Route::get( 'privacy-policy', function ()
     return view( 'privacy' );
 })->name( 'privacy' );
 
-Auth::routes();
-
-Route::get('/', 'HomeController@index');
+Route::get('profile', function ()
+{
+    return view( 'profile' )
+})->middleware('auth');
