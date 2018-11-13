d7ecacdacb6c8d793e380a547913550c38582f30
Felix Wetell <felix.wetell@gmail.com>
2018-11-13 19:40:29 +0100

    Update database/factories/UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => 'SecretSafe!2', // secret
         'remember_token' => str_random(10),
     ];
 });
