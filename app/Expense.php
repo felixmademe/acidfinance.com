4554aa42488e24e5285b780828cf50395e66e279
Felix Wetell <felix.wetell@gmail.com>
2018-11-19 20:15:33 +0100

    Added relationship to category

--- app/Expense.php
+++ app/Expense.php
@@ -2,12 +2,15 @@
 
 namespace App;
 
-use App\Transaction;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Transaction
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo( 'Category' );
+    }
 
     protected $table = 'expenses';
 }
--- app/Income.php
+++ app/Income.php
@@ -2,12 +2,15 @@
 
 namespace App;
 
-use App\Transaction;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Income extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo( 'Category' );
+    }
 
     protected $table = 'incomes';
 }
