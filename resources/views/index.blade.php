81bf92f352eedc50a08e769e0137f71e2cbcf477
Felix Wetell <felix.wetell@gmail.com>
2018-11-19 19:48:47 +0100

    Started designing main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,66 @@
 @section( 'content' )
 
     @auth
-        <h1>
-            {{ Auth::user()->username }}
-        </h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="text-center">
+                    <h1>
+                        {{ date( 'F' ) }}
+                    </h1>
+                </div>
+                <hr><br>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset( '/img/total.svg' ) }}" alt="Equal icon in cirlce on a one colour background">
+                    <div class="card-body text-center">
+                        <h4>Total</h4>
+                        {{-- TODO: if-statment, green if money left, red if money in minus --}}
+                        <p class="sum">20213kr left this month</p>
+                    </div>
+                </div>
+                <br><hr><br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <img class="card-img-top" src="{{ asset( '/img/income.svg' ) }}" alt="Plus icon in cirlce on a one colour background">
+                            <div class="card-body">
+                                <h4 class="text-center">Income</h4>
+                                <hr>
+                                <ul class="list-group">
+                                    <li class="list-group-item border-0">
+                                        Total - <span class="green-text">2310123 kr</span>
+                                    </li>
+                                    <li class="list-group-item border-0">
+                                        Source - <span class="green-text">2000 kr</span>
+                                    </li>
+                                    <li class="list-group-item border-0">
+                                        Source - <span class="green-text">2000 kr</span>
+                                    </li>
+                                </ul>
+                                <a href="{{ route( 'expense' ) }}" class="btn btn-orange">Edit</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <img class="card-img-top" src="{{ asset( '/img/expense.svg' ) }}" alt="Minus icon in cirlce on a one colour background">
+                            <div class="card-body">
+                                <h4 class="text-center">Expense</h4>
+                                <hr>
+                                <ul class="list-group">
+                                    <li class="list-group-item border-0">
+                                        Total - <span class="red-text">2310123 kr</span>
+                                    </li>
+                                    <li class="list-group-item border-0">
+                                        Source - <span class="red-text">2000 kr</span>
+                                    </li>
+                                </ul>
+                                <a href="" class="btn btn-orange">Edit</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     @else
         <div class="jumbotron">
             <h1>Simplifying finance</h1>
