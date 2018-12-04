9378011317ddc1136f4c9c879060ded23430b507
Felix Wetell <felix.wetell@gmail.com>
2018-12-04 17:32:55 +0100

    Update MonthController.php

--- app/Http/Controllers/MonthController.php
+++ app/Http/Controllers/MonthController.php
@@ -9,6 +9,7 @@ use Auth;
 use App\User;
 use App\Income;
 use App\Expense;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -34,25 +35,46 @@ class MonthController extends Controller
         $currentMonth = date( 'm' );
         $user = Auth::user();
         $incomes = Income::where( 'user_id', $user->id )
-                         ->whereMonth( 'created_at', $currentMonth )
-                         ->orderBy( 'amount', 'desc' )
-                         ->limit( 3 )
-                         ->get();
+            ->whereMonth( 'created_at', $currentMonth )
+            ->orderBy( 'amount', 'desc' )
+            ->limit( 3 )
+            ->get();
 
         $expenses = Expense::where( 'user_id', $user->id )
-                           ->whereMonth( 'created_at', $currentMonth )
-                           ->orderBy( 'amount', 'desc' )
-                           ->limit( 3 )
-                           ->get();
+            ->whereMonth( 'created_at', $currentMonth )
+            ->orderBy( 'amount', 'desc' )
+            ->limit( 3 )
+            ->get();
 
         return view( 'index' )
-                ->with( 'incomes', $incomes )
-                ->with( 'expenses', $expenses )
-                ->with( 'currentMonth', $currentMonth );
+            ->with( 'incomes', $incomes )
+            ->with( 'expenses', $expenses )
+            ->with( 'currentMonth', $currentMonth );
     }
 
     public function previous()
     {
-        return view( 'previous' );
+        $user = Auth::user();
+        $incomes = $user->incomes;
+        $expenses = $user->expenses;
+
+        $transactions = $incomes->merge( $expenses );
+
+        $transactionsByYearMonth = $transactions->groupBy([
+            function( $date )
+                {
+                    return Carbon::parse( $date->created_at )->format( 'Y' );
+                },
+                function( $date )
+                {
+                    return Carbon::parse( $date->created_at )->format( 'F' );
+                }
+        ]);
+
+
+        return view( 'previous' )
+            ->with( 'transactionsByYearMonth', $transactionsByYearMonth )
+            ->with( 'incomes', $incomes )
+            ->with( 'expenses', $expenses );
     }
 }
