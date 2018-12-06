59c2a4c5a86f42ce9005e77d067008bbdb0d4333
Felix Wetell <felix.wetell@gmail.com>
2018-12-06 19:20:56 +0100

    Forcing https if app is in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if( env( 'APP_ENV' ) == 'production' )
+        {
+            URL::forceScheme( 'https' );
+            $this->app[ 'request' ]->server->set( 'HTTPS', true );
+        }
     }
 
     /**
