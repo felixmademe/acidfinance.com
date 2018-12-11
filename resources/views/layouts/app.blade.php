48ae20cc45e74af19c691505994af36cf65d01fc
Felix Wetell <felix.wetell@gmail.com>
2018-12-11 13:27:19 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,8 +95,10 @@
                                 {{ __( 'Profile' ) }}
                             </a>
                             <a class="dropdown-item" href="{{ route( 'logout' ) }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                               onclick="
+                                    event.preventDefault();
+                                    document.getElementById('logout-form').submit();
+                                ">
                                 {{ __('Logout') }}
                             </a>
 
