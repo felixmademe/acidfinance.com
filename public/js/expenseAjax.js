1f8fe3eaa92c7bf2439844cb6873bfee63cb5bde
Felix Wetell <felix.wetell@gmail.com>
2019-01-17 10:34:49 +0100

    Restructured ajax code

---
+++ public/js/expenseAjax.js
@@ -0,0 +1,66 @@
+$.ajaxSetup(
+{
+    headers:
+    {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+} );
+
+$( '.expense-form' ).on( 'submit', function( e )
+{
+    e.preventDefault();
+    e.stopPropagation();
+
+    var form = $(this);
+    var id = form.children( "input[name='id']" ).val();
+    var row = form.parent().parent();
+    let ajax = $.ajax(
+    {
+        type: 'DELETE',
+        url: '/expense/' + id,
+        data:
+        {
+            id: id,
+        },
+        dataType: 'html',
+        success: function( data )
+        {
+            console.log( data );
+            $( 'div.flash-message' ).html( data ).fadeIn( 400 );
+            row.remove();
+        },
+        error: function( data )
+        {
+            console.log(data);
+            $( 'div.flash-message' ).html( data ).fadeIn( 400 );
+        },
+    } );
+} );
+
+$( '.addExpense' ).on( 'submit', function( e )
+{
+    e.preventDefault();
+    e.stopPropagation();
+
+    var form = $( this );
+    let ajax = $.ajax(
+    {
+        type: 'POST',
+        async: false,
+        url: '/expense',
+        dataType: 'json',
+        success: function( data )
+        {
+            console.log( data );
+            console.log( data.expenseView );
+            expenseView = data.expenseView;
+            $( 'div.flash-message' ).html( data.message ).fadeIn( 400 );
+            $( '.table tbody' ).append( expenseView );
+        },
+        error: function( data )
+        {
+            console.log( data );
+            $( 'div.flash-message' ).html( data.html ).fadeIn( 400 );
+        },
+    } );
+} );
