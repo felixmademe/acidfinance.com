b0d6facc17bf512addadb8aee07d36d673d0e3a4
Felix Wetell <felix.wetell@gmail.com>
2019-03-12 13:37:25 +0100

    Removed some code #11

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,62 +1,38 @@
-@component('mail::message')
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level === 'error')
-# @lang('Whoops!')
-@else
-# @lang('Hello!')
-@endif
-@endif
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <style>
+            @font-face
+            {
+                font-family: 'Comfortaa', 'Raleway', sans-serif !important;
+                src: url('https://fonts.googleapis.com/css?family=Comfortaa|Raleway');
+            }
+            a
+            {
+                color: #00C851 !important;
+                text-decoration: none !important;
+            }
+            body
+            {
+                border: 10px;
+                margin: 20px auto !important;
+            }
+            h1
+            {
+                color: #00C851 !important;
+                text-align: center !important;
+            }
 
-{{-- Intro Lines --}}
-@foreach ($introLines as $line)
-{{ $line }}
-
-@endforeach
-
-{{-- Action Button --}}
-@isset($actionText)
-<?php
-    switch ($level) {
-        case 'success':
-        case 'error':
-            $color = $level;
-            break;
-        default:
-            $color = 'primary';
-    }
-?>
-@component('mail::button', ['url' => $actionUrl, 'color' => $color])
-{{ $actionText }}
-@endcomponent
-@endisset
-
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-{{ $line }}
-
-@endforeach
-
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-@lang('Regards'),<br>{{ config('app.name') }}
-@endif
-
-{{-- Subcopy --}}
-@isset($actionText)
-@component('mail::subcopy')
-@lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: [:actionURL](:actionURL)',
-    [
-        'actionText' => $actionText,
-        'actionURL' => $actionUrl,
-    ]
-)
-@endcomponent
-@endisset
-@endcomponent
+        </style>
+    </head>
+    <body>
+        <h1>Acid Finance</h1>
+        <p>Simplifying and visualising finance
+            <br><br>
+            {{ $actionText }}
+            <br><br>
+            <a href="{{ $actionUrl }}">Click here</a>
+        </p>
+    </body>
+</html>
